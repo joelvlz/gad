04a05e4f45831ea200700652ab3138703c7484e1
Elsorbo <aron_1991@live.com.ar>
2018-02-26 18:25:33 -0500

    Arrow functions modificadas

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 var spam_menu = document.getElementById('spam_menu'),
 	nav = document.getElementById('nav');
 
-spam_menu.addEventListener('click',()=>{
+spam_menu.addEventListener('click',function (){
 	nav.classList.toggle('ver');
 },false);
